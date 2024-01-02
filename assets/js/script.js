bec535dd77e9023040f6ba9465fd1454806fde20
BwhelanKK <>
2024-01-02 15:37:20 +0000

    Feat:Display gameType questions and alerts

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,18 +3,35 @@ document.addEventListener("DOMContentLoaded", function () {
 
     for (let button of buttons) {
         button.addEventListener("click", function () {
-            if (this.getAttribute(data - type) === "submit") {
+            if (this.getAttribute("data-type") === "submit") {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+
+    runGame("addition");
+
 });
 
+/** 
+ * The main game "loop", called when the script is first loaded
+ * and after the users answer has been processed
+*/
+function runGame(gameType) {
 
-function runGame() {
+    // Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25 + 1);
+    let num2 = Math.floor(Math.random() * 25 + 1);
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 
 }
 
