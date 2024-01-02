53e0031244569188581f1b43db83fc3bc3c242bf
BwhelanKK <>
2024-01-02 18:25:31 +0000

    Update:Fix code to check the answer and respond to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,26 +37,10 @@ function runGame(gameType) {
     }
 
 }
+
 /**
  * Checks the element against the first element in
  * the returned calculateCorrectAnswer array
- */
-function checkAnswer() {
-
-    let userAnswer = parseInt(document.getElementById("answer-box").value);
-    let calculatedAnswer = calculateCorrectAnswer();
-    let isCorrect = userAnswer === calculatedAnswer[0];
-
-    if (isCorrect) {
-        alert("Hey! You got it right! :D");
-    } else {
-        alert(`Awww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}`);
-    }
-}
-
-/**
- * Gets the operand (the numbers) and the operatot (plus, minus etc)
- * directly from the DOM, and returns the correct answer.
  */
 function checkAnswer() {
 
@@ -73,6 +57,27 @@ function checkAnswer() {
     runGame(calculatedAnswer[1]);
 
 }
+
+/**
+ * Gets the operand (the numbers) and the operatot (plus, minus etc)
+ * directly from the DOM, and returns the correct answer.
+ */
+function calculateCorrectAnswer() {
+
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById("operator").innerText;
+
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
+    }
+
+}
+
+
 
 function incrementScore() {
 
